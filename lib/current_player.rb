9615dc7791477af3fcef56909f23e19b09b7609c
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-11 00:00:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,11 +11,13 @@ count
 end
 
 def current_player(board)
-  # if turn_count(board)
+  # turn_count(board)
+  count = 0
   board.each do |player|
     if player == "X"
       player
-
+count += 1
     end
   end
+  count
 end
